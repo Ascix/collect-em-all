8e5145bb2dbf070aca017b5bfa13bb2b3371d3e0
Brice Tran <brice1992@msn.com>
2022-07-22 16:58:59 -0400

    fixed bug where incorrect name was shown on first log in

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ import {
 } from "firebase/database";
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import BlockedSpaces from "./components/BlockedSpaces";
 import ChatboxScroll from "./components/ChatboxScroll";
 import ChatMessage from "./components/ChatMessage";
@@ -39,88 +39,104 @@ function App() {
     messagingSenderId: "485338782363",
     appId: "1:485338782363:web:8164385fc4506beebb443e",
   };
-  
+
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const database = getDatabase(app);
-  
+
+  //player system
   const [players, setPlayers] = useState({});
-  const [addedPlayer, setAddedPlayer] = useState([])
+  const [addedPlayer, setAddedPlayer] = useState([]);
   const [playerId, setPlayerId] = useState(null);
   const [playerRef, setPlayerRef] = useState(null);
-  const [name, setName] = useState('')
-  
-  const [coins, setCoins] = useState({});
 
-  const [skins, setSkins] = useState({});
+  function InitGame() {
+    const allPlayersRef = query(
+      ref(database, `players`),
+      orderByChild("loggedIn"),
+      equalTo(true)
+    );
+    const allCoinsRef = ref(database, `coins`);
+    const allChatRef = query(ref(database, `chat`), limitToLast(10));
 
-  const [chat, setChat] = useState(null);
-
-  const [text, setText] = useState('')
-  
-  const keypress = useRef(false);
-  
-  const [show, setShow] = useState(false);
-  const [color, setColor] = useState('')
-  const [colorOwned, setColorOwned] = useState(false)
-  const [disable, setDisable] = useState(false)
-
-  const handleClose = () => setShow(false);
-  
-  const handleYes = (() => {
-    setShow(false)
-    if(players[playerId].skins[color] !== true) {
-      update(playerRef, {
-        color: color,
-        skins: {
-          ...players[playerId].skins,
-          [color]: true
-        },
-        coins: players[playerId].coins - 100
-      });
-      setSkins(players[playerId].skins)
-    }
-    else {
-      update(playerRef, {
-        color: color
-      });
-    }
-  })
-  
-  function Skins(props) {
-    const handleShow = (() => {
-      setShow(true)
-      setColor(color)
-      setColorOwned(players[playerId].skins[color])
-      if (players[playerId].skins[color] !== "true" && (players[playerId].coins) < 100) {
-        setDisable(true)
-      }
-    })
-    const { color, owned } = props
-    return(
-      <>
-            <button className={"skin " + owned} onClick={handleShow} color={color}></button>          
-      </>
-    )
-}
-
-  function GrabCoin(x, y) {
-    const key = GetCoordinates(x, y);
-    if (coins[key]) {
-      remove(ref(database, `coins/${key}`));
-      update(playerRef, {
-        coins: players[playerId].coins + 1,
-      });
-      get(ref(database, `coins`)).then((snapshot) => {
-        const coins = snapshot.val();
-        if (Object.keys(coins).length < 10) {
-          CoinSpawn();
-        }
-      });
-    }
+    onValue(allPlayersRef, (snapshot) => {
+      setPlayers(snapshot.val());
+    });
+    onChildAdded(allPlayersRef, (snapshot) => {
+      setAddedPlayer(snapshot.val());
+    });
+    onValue(allCoinsRef, (snapshot) => {
+      setCoins(snapshot.val());
+    });
+    onValue(allChatRef, (snapshot) => {
+      setChat(snapshot.val());
+    });
+    CoinSpawn();
   }
 
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        // logged in
+
+        setPlayerId(user.uid);
+
+        const player = ref(database, `players/${user.uid}`);
+        setPlayerRef(player);
+
+        get(player).then((snapshot) => {
+          const playerData = snapshot.val() || {};
+          set(player, {
+            id: user.uid,
+            name: playerData.name || CreateName(),
+            direction: playerData.direction || "right",
+            color: playerData.color || "blue",
+            x: playerData.x || 3,
+            y: playerData.y || 5,
+            coins: playerData.coins || 0,
+            loggedIn: true,
+            DigitalCrafts: playerData.DigitalCrafts || false,
+            skins: {
+              blue: playerData.skins?.blue || true,
+              red: playerData.skins?.red || false,
+              orange: playerData.skins?.orange || false,
+              yellow: playerData.skins?.yellow || false,
+              green: playerData.skins?.green || false,
+              purple: playerData.skins?.purple || false,
+            },
+          });
+          setSkins(playerData.skins);
+          setName(playerData.name || name);
+        });
+
+        // set character log in status to false on disconnect
+        onDisconnect(player).update({
+          loggedIn: false,
+        });
+
+        InitGame();
+        refer.current.focus();
+      }
+    });
+    signInAnonymously(auth);
+  }, []);
+
+  //name change system
+  const [name, setName] = useState('');
+
+  const handleChange = (e) => {
+    update(playerRef, {
+      name: e.target.value,
+    });
+    setName(e.target.value);
+  };
+  useEffect(() => {
+    setName(players[playerId]?.name);
+  }, [name]);
+
   // player movement
+  const keypress = useRef(false);
+
   useEffect(() => {
     if (!playerRef) {
       return;
@@ -167,191 +183,216 @@ function App() {
     };
   }, [players, playerRef, playerId]);
 
-  function InitGame() {
-    const allPlayersRef = query(
-      ref(database, `players`),
-      orderByChild("loggedIn"),
-      equalTo(true)
-    );
-    const allCoinsRef = ref(database, `coins`);
-    const allChatRef = query(
-      ref(database, `chat`),
-      limitToLast(10)
-    );
-
-    onValue(allPlayersRef, (snapshot) => {
-      setPlayers(snapshot.val());
-    });
-    onChildAdded(allPlayersRef, (snapshot) => {
-      setAddedPlayer(snapshot.val())
-    })
-    onValue(allCoinsRef, (snapshot) => {
-      setCoins(snapshot.val());
-    });
-    onValue(allChatRef, (snapshot) => {
-      setChat(snapshot.val());
-    });
-    CoinSpawn();
-  }
-  useEffect(() => {
-    setTimeout(ChatboxScroll(), 1)
-  },[chat])
-  useEffect(() => {
-    setSkins(players[playerId]?.skins);
-  },[show])
-
   const refer = useRef(null);
 
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        // logged in
+  //coin system
+  const [coins, setCoins] = useState({});
 
-        setPlayerId(user.uid);
+  function GrabCoin(x, y) {
+    const key = GetCoordinates(x, y);
+    if (coins[key]) {
+      remove(ref(database, `coins/${key}`));
+      update(playerRef, {
+        coins: players[playerId].coins + 1,
+      });
+      get(ref(database, `coins`)).then((snapshot) => {
+        const coins = snapshot.val();
+        if (Object.keys(coins).length < 10) {
+          CoinSpawn();
+        }
+      });
+    }
+  }
 
-        const player = ref(database, `players/${user.uid}`);
-        setPlayerRef(player);
-        const name = CreateName();
-
-        get(player).then((snapshot) => {
-          const playerData = snapshot.val() || {};
-          set(player, {
-            id: user.uid,
-            name: playerData.name || name,
-            direction: playerData.direction || "right",
-            color: playerData.color || "blue",
-            x: playerData.x || 3,
-            y: playerData.y || 5,
-            coins: playerData.coins || 0,
-            loggedIn: true,
-            DigitalCrafts: playerData.DigitalCrafts || false,
-            skins: {
-              blue: playerData.skins?.blue || true,
-              red: playerData.skins?.red || false,
-              orange: playerData.skins?.orange || false,
-              yellow: playerData.skins?.yellow || false,
-              green: playerData.skins?.green || false,
-              purple: playerData.skins?.purple || false,
-            }
-          });
-          setSkins(playerData.skins)
-          setName(playerData.name || name)
-        });
-
-        
-        // set character log in status to false on disconnect
-        onDisconnect(player).update({
-          loggedIn: false,
-        });
-        
-        InitGame();
-        refer.current.focus();
-      }
-    });
-    signInAnonymously(auth);
-  }, []);
-  
-  const handleChange = (e) => {
-    update(playerRef, {
-      name: e.target.value,
-    });
-    setName(e.target.value)
-  };
+  //chat system
+  const [chat, setChat] = useState(null);
+  const [text, setText] = useState("");
 
   const handleText = (e) => {
-    setText(e.target.value)
+    setText(e.target.value);
   };
 
+  useEffect(() => {
+    setTimeout(ChatboxScroll(), 1);
+  }, [chat]);
+
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if (text === "$DigitalCrafts")  {
+    e.preventDefault();
+    if (text === "$DigitalCrafts") {
       if (players[playerId].DigitalCrafts === false) {
         update(playerRef, {
           coins: players[playerId].coins + 1000,
-          DigitalCrafts: true
+          DigitalCrafts: true,
         });
       }
-    }
-    else {
-      const chatRef = ref(database, `chat/${new Date()}`)
+    } else {
+      const chatRef = ref(database, `chat/${new Date()}`);
       set(chatRef, {
         name: name.toUpperCase(),
-        message: text
-      })
+        message: text,
+      });
     }
-    setText("")
+    setText("");
+  };
+
+  //skin system
+  const [skins, setSkins] = useState({});
+  const [show, setShow] = useState(false);
+  const [color, setColor] = useState("");
+  const [colorOwned, setColorOwned] = useState(false);
+  const [disable, setDisable] = useState(false);
+  const handleClose = () => setShow(false);
+
+  const handleYes = () => {
+    setShow(false);
+    if (players[playerId].skins[color] !== true) {
+      update(playerRef, {
+        color: color,
+        skins: {
+          ...players[playerId].skins,
+          [color]: true,
+        },
+        coins: players[playerId].coins - 100,
+      });
+      setSkins(players[playerId].skins);
+    } else {
+      update(playerRef, {
+        color: color,
+      });
+    }
+  };
+
+  //render skins
+  function Skins(props) {
+    const handleShow = () => {
+      setShow(true);
+      setColor(color);
+      setColorOwned(players[playerId].skins[color]);
+      if (
+        players[playerId].skins[color] !== "true" &&
+        players[playerId].coins < 100
+      ) {
+        setDisable(true);
+      }
+    };
+    const { color, owned } = props;
+    return (
+      <>
+        <button
+          className={"skin " + owned}
+          onClick={handleShow}
+          color={color}
+        ></button>
+      </>
+    );
   }
+  useEffect(() => {
+    setSkins(players[playerId]?.skins);
+  }, [show]);
 
   return (
     <div className="App">
       <div className="ui">
         <div className="left">
           <div className="player-name">
-            <label htmlFor="player-name">Name</label><br></br>
-            <input id="player-name" value={name} maxLength={10} onChange={handleChange} autoComplete="off"/>
+            <label htmlFor="player-name">Name</label>
+            <br></br>
+            <input
+              id="player-name"
+              value={name}
+              maxLength={10}
+              onChange={handleChange}
+              autoComplete="off"
+            />
           </div>
           <div className="chat">
             <div className="messages" id="chatbox">
-            {chat && 
-              Object.entries(chat ?? {}).map(([key, message]) => {
-                return (
-                  <ChatMessage
-                    key={key}
-                    name={message.name}
-                    message={message.message}
-                  />
-              );
-            })}
+              {chat &&
+                Object.entries(chat ?? {}).map(([key, message]) => {
+                  return (
+                    <ChatMessage
+                      key={key}
+                      name={message.name}
+                      message={message.message}
+                    />
+                  );
+                })}
             </div>
             <form onSubmit={handleSubmit} className="message-input">
-              <input id="chat" value={text} maxLength={30} onChange={handleText} autoComplete="off"/>
+              <input
+                id="chat"
+                value={text}
+                maxLength={30}
+                onChange={handleText}
+                autoComplete="off"
+              />
             </form>
           </div>
         </div>
-        <div className="skins">
-        {skins &&
-          Object.entries(skins ?? {}).map(([key, skin]) => {
-            return (
-              <div key={key}>
-              <Skins
-                color={key}
-                owned={skin}
-                  />
-              </div>
-            )
-          })
-          }
+        <div className="skin-ui">
+          <h4>
+            Skins
+          </h4>
+          <div className="skins">
+          {skins &&
+            Object.entries(skins ?? {}).map(([key, skin]) => {
+              return (
+                <div key={key}>
+                  <Skins color={key} owned={skin} />
+                </div>
+              );
+            })}
           <Modal show={show} onHide={handleClose}>
-              {colorOwned ? 
-              <><Modal.Header closeButton>
+            {colorOwned ? (
+              <>
+                <Modal.Header closeButton>
                   <Modal.Title>Change Skin?</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Would you like to change to this skin?</Modal.Body></> : 
-              <><Modal.Header closeButton>
+                <Modal.Body>Would you like to change to this skin?</Modal.Body>
+              </>
+            ) : (
+              <>
+                <Modal.Header closeButton>
                   <Modal.Title>Buy Skin?</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>You do not own this skin. Would you like to buy it for 100 coins?</Modal.Body></>
-              }
-              <Modal.Footer>
-              {colorOwned ? 
-              <><Button variant="success" onClick={handleYes}>
-                Yes
-              </Button>
-              <Button variant="danger" onClick={handleClose}>
-                No
-              </Button></> : 
-              !disable ? <><Button variant="success" onClick={handleYes} disabled={disable}>
-                Yes
-              </Button>
-              <Button variant="danger" onClick={handleClose}>
-                No
-              </Button></> : 
-              <Button variant="danger" onClick={handleClose}>
-                You do not have enough coins!
-              </Button>
-              }
-              </Modal.Footer>
-        </Modal>
+                <Modal.Body>
+                  <Skins color={color} /><br></br>
+                  You do not own this skin. Would you like to buy it for 100
+                  coins?
+                </Modal.Body>
+              </>
+            )}
+            <Modal.Footer>
+              {colorOwned ? (
+                <>
+                  <Button variant="success" onClick={handleYes}>
+                    Yes
+                  </Button>
+                  <Button variant="danger" onClick={handleClose}>
+                    No
+                  </Button>
+                </>
+              ) : !disable ? (
+                <>
+                  <Button
+                    variant="success"
+                    onClick={handleYes}
+                    disabled={disable}
+                  >
+                    Yes
+                  </Button>
+                  <Button variant="danger" onClick={handleClose}>
+                    No
+                  </Button>
+                </>
+              ) : (
+                <Button variant="danger" onClick={handleClose}>
+                  You do not have enough coins!
+                </Button>
+              )}
+            </Modal.Footer>
+          </Modal>
+          </div>
         </div>
       </div>
       <div className="game-container" ref={refer}>
@@ -366,14 +407,10 @@ function App() {
                 direction={player.direction}
                 left={16 * player.x + "px"}
                 top={16 * player.y - 4 + "px"}
-                character={
-                  (playerId === key) ?  "character you" : "character" 
-                  }
-                  />
-            )
-          }
-            )
-          }
+                character={playerId === key ? "character you" : "character"}
+              />
+            );
+          })}
         {Object.entries(coins ?? {}).map(([key, coin]) => {
           return (
             <RenderCoin
